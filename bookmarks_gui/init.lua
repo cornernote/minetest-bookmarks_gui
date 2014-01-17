de4df31dbfb4879154ad709e72a260910f4b8645
Brett O'Donnell <cornernote@gmail.com>
2014-01-17 11:01:49 +1030

    Update init.lua

--- bookmarks_gui/init.lua
+++ bookmarks_gui/init.lua
@@ -3,8 +3,8 @@
 Bookmarks GUI for Minetest
 
 Copyright (c) 2012 cornernote, Brett O'Donnell <cornernote@gmail.com>
-Source Code: https://github.com/cornernote/minetest-particles
-License: GPLv3
+Source Code: https://github.com/cornernote/minetest-bookmarks_gui
+License: BSD-3-Clause https://raw.github.com/cornernote/minetest-bookmarks_gui/master/LICENSE
 
 ]]--
 
